9ecb0b3cd9fbcab55d39d6af1845ab73f6c2403a
mvaibhav06 <55031067+mvaibhav06@users.noreply.github.com>
2024-06-20 08:35:33 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int out = 0;
+        
+        for(int i=nums.length-1; i>1; i--){
+            for(int j=i-1; j>0; j--){
+                for(int k=j-1; k>=0; k--){
+                    int a = nums[i];
+                    int b = nums[j];
+                    int c = nums[k];
+                    
+                    if((a < b+c) && (b < a+c) && (c < a+b)){
+                        out = a+b+c;
+                        return out;
+                    }
+                }
+            }
+        }
+
+        return out;
+        
+    }
+}
